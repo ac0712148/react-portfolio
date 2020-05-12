aae0ef134265f31549b50e039a2bf5ba5a38eed0
Albert Cervantes <cervanalbert@gmail.com>
2020-05-12 00:50:01 -0700

    added basic layout App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'antd/dist/antd.css';
+import './assets/css/App.css';
+import {Layout, Menu, Breadcrumb} from 'antd';
+import {MailOutlined, CodeOutlined, HomeOutlined, UserOutlined, AppstoreOutlined} from '@ant-design/icons';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const {Header, Content, Footer, Sider} = Layout;
+const {SubMenu} = Menu;
+
+class App extends React.Component {
+    state = {
+        collapsed: false
+    };
+
+    onCollapse = collapsed => {
+        console.log(collapsed);
+        this.setState({collapsed});
+    };
+
+    render() {
+        return (
+            <Layout style={{
+                minHeight: '100vh'
+            }}>
+                <Sider
+                    collapsible
+                    collapsed={this.state.collapsed}
+                    onCollapse={this.onCollapse}
+                    width="138px"
+                    >
+                    <div className="logo"/>
+                    <Menu className="side-color" theme="dark" defaultSelectedKeys={['1']} mode="inline" >
+                        <Menu.Item key="1" icon={< HomeOutlined />}>
+                            HOME
+                        </Menu.Item>
+                        <Menu.Item key="2" icon={< UserOutlined />}>
+                            ABOUT
+                        </Menu.Item>
+                        <Menu.Item key="3" icon={< CodeOutlined />}>
+                            PROJECTS
+                        </Menu.Item>
+                        <Menu.Item key="4" icon={< MailOutlined />}>
+                            CONTACT
+                        </Menu.Item>
+                    </Menu>
+                </Sider>
+                <Layout className="site-layout">
+                    <Header
+                        className="site-layout-background"
+                        style={{
+                        padding: 0
+                    }}/>
+                    <Content
+                        style={{
+                        margin: '16px 16px'
+                    }}>
+                        Test Text
+                    </Content>
+                    <Footer
+                        style={{
+                        textAlign: 'center'
+                    }}>fOOTER</Footer>
+                </Layout>
+            </Layout>
+        );
+    }
 }
 
-export default App;
+export default App
