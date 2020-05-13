e0c96874a3e945226c4faa246de9f1c6b3ade27c
Albert Cervantes <cervanalbert@gmail.com>
2020-05-13 03:02:19 -0700

    added css arrows

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,22 +1,40 @@
 import React from "react";
-import {Card} from "antd";
 import {red} from '@ant-design/colors';
+import "../assets/css/Home.css"
 
 class Home extends React.Component {
     render() {
         return(
-            <div className="home">
-                <Card
-                    bordered={false}
-                    style={{
-                    width: 300,
-                    backgroundColor: "#2a1215",
-                    color: "white"
-                }}>
-                    <p>Card content</p>
-                    <p>Card content</p>
-                    <p>Card content</p>
-                </Card>
+            // style={{backgroundColor: "#431418"}}
+            <div className="home" >
+                <div className="hometext">
+                    <p className="headline"> Welcome, I'm Albert</p>
+                    <p className="subtext" > Web/Software Developer</p>
+                    <div className="arrow">
+                        <p className="text1" style={{backgroundColor: "#2a1215"}}><span className="test">Test</span></p>
+                        <p className="text2" style={{backgroundColor: "#431418"}}><span className="test">Test</span></p>
+                        <p className="text3" style={{backgroundColor: "#58181c"}}><span className="test">Test</span></p>
+                        <p className="text4" style={{backgroundColor: "#791a1f"}}><span className="test">Test</span></p>
+                        <p className="text5" style={{backgroundColor: "#a61d24"}}><span className="test">Test</span></p>
+                        <p className="text6" style={{backgroundColor: "#d32029"}}><span className="test">Test</span></p>
+                        <p className="text7" style={{backgroundColor: "#e84749"}}><span className="test">Test</span></p>
+                        <p className="text8" style={{backgroundColor: "#f37370"}}><span className="test">Test</span></p>
+                        <p className="text9" style={{backgroundColor: "#f89f9a"}}><span className="test">Test</span></p>
+                        <p className="text10" style={{backgroundColor: "#fac8c3"}}><span className="test">Test</span></p>
+                    </div>
+                    <div className="arrow">
+                        <p className="text1" style={{backgroundColor: "#2a1215"}}><span className="test">Test</span></p>
+                        <p className="text2" style={{backgroundColor: "#431418"}}><span className="test">Test</span></p>
+                        <p className="text3" style={{backgroundColor: "#58181c"}}><span className="test">Test</span></p>
+                        <p className="text4" style={{backgroundColor: "#791a1f"}}><span className="test">Test</span></p>
+                        <p className="text5" style={{backgroundColor: "#a61d24"}}><span className="test">Test</span></p>
+                        <p className="text6" style={{backgroundColor: "#d32029"}}><span className="test">Test</span></p>
+                        <p className="text7" style={{backgroundColor: "#e84749"}}><span className="test">Test</span></p>
+                        <p className="text8" style={{backgroundColor: "#f37370"}}><span className="test">Test</span></p>
+                        <p className="text9" style={{backgroundColor: "#f89f9a"}}><span className="test">Test</span></p>
+                        <p className="text10" style={{backgroundColor: "#fac8c3"}}><span className="test">Test</span></p>
+                    </div>
+                </div>
             </div>
         )
     }
