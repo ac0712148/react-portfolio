7ececb14a1a1a3fd8a211493af99f36a05f65e1e
Albert Cervantes <cervanalbert@gmail.com>
2020-05-12 20:16:40 -0700

    added side menu component to layout

--- src/App.js
+++ src/App.js
@@ -1,60 +1,26 @@
 import React from 'react';
 import 'antd/dist/antd.css';
 import './assets/css/App.css';
-import {Layout, Menu, Breadcrumb} from 'antd';
-import {MailOutlined, CodeOutlined, HomeOutlined, UserOutlined, AppstoreOutlined} from '@ant-design/icons';
+import {Layout} from 'antd';
+import SideMenu from "./components/SideMenu"
 
-const {Header, Content, Footer, Sider} = Layout;
-const {SubMenu} = Menu;
+const {Content, Footer} = Layout;
 
 class App extends React.Component {
-    state = {
-        collapsed: false
-    };
-
-    onCollapse = collapsed => {
-        console.log(collapsed);
-        this.setState({collapsed});
-    };
 
     render() {
         return (
             <Layout style={{
                 minHeight: '100vh'
             }}>
-                <Sider
-                    collapsible
-                    collapsed={this.state.collapsed}
-                    onCollapse={this.onCollapse}
-                    width="138px"
-                    >
-                    <div className="logo"/>
-                    <Menu className="side-color" theme="dark" defaultSelectedKeys={['1']} mode="inline" >
-                        <Menu.Item key="1" icon={< HomeOutlined />}>
-                            HOME
-                        </Menu.Item>
-                        <Menu.Item key="2" icon={< UserOutlined />}>
-                            ABOUT
-                        </Menu.Item>
-                        <Menu.Item key="3" icon={< CodeOutlined />}>
-                            PROJECTS
-                        </Menu.Item>
-                        <Menu.Item key="4" icon={< MailOutlined />}>
-                            CONTACT
-                        </Menu.Item>
-                    </Menu>
-                </Sider>
-                <Layout className="site-layout">
-                    <Header
-                        className="site-layout-background"
-                        style={{
-                        padding: 0
-                    }}/>
-                    <Content
-                        style={{
-                        margin: '16px 16px'
-                    }}>
-                        Test Text
+                <SideMenu/>
+                <Layout
+                    className="site-layout"
+                    style={{
+                    minHeight: '100vh'
+                }}>
+                    <Content >
+                        <div className="content" >Test Text Hello World</div>
                     </Content>
                     <Footer
                         style={{
